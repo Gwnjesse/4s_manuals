2a73c503f37c6d956b41ac0343269c7f76f8612d
Gwnjesse <156432610+Gwnjesse@users.noreply.github.com>
2024-10-14 11:20:58 +0200

    Update manual_list.blade.php

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -8,23 +8,18 @@
         <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
-
     <h1>{{ $brand->name }}</h1>
 
     <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
-    <table>
-        <tr>
-           @foreach ($manuals as $manual)
-                    <td style="border: black solid 3px;">
-                        <a href="{{ route('manual.redirect', $manual->id) }}" alt="{{ $manual->name }}" title="{{ $manual->name }}" target="{{ $manual->locally_available ? '_self' : '_blank' }}">
-                            {{ $manual->name }}
-                        </a>
-                        ({{$manual->filesize_human_readable}})
-                    </td>
-                <br />
-            @endforeach
-
-        </tr>
-    </table>
+    <div class="manuals">
+        @foreach ($manuals as $manual)
+            <div class="itemsmanual">
+                <a href="{{ route('manual.redirect', $manual->id) }}" alt="{{ $manual->name }}" title="{{ $manual->name }}" target="{{ $manual->locally_available ? '_self' : '_blank' }}">
+                    {{ $manual->name }}
+                </a>
+                ({{$manual->filesize_human_readable}})
+            </div>
+        @endforeach
+    </div>
 </x-layouts.app>
