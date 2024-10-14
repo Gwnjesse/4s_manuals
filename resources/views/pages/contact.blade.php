f614d26683120fa11dae65e17135b82ae7e0db81
the_shy_developer <pickcrafterplayer1@gmail.com>
2024-10-14 11:37:56 +0200

    Create contact.blade.php

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Contactgegevens</h1>
+        <p>Naam bedrijf: Legacy-app</p>
+        <p>Telefoonnummer: 06 91057205</p>
+        <p>Adres: 5111 XC, Baarle-Nassau</p><br>
+
+        <p>Wil je een mail sturen? Dat kan hier in dit veld hieronder:</p>
+        <textarea></textarea><br>
+        <input type="submit" value="Versturen"><br>
+
+        <a href="/">Terug naar home</a>
+    </body>
+</html>
